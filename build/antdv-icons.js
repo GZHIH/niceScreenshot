9e438144d576c0fc953a7d818a0e4d5572a8b96e
ApexLegend <cutelittlecat@163.com>
2022-09-29 15:58:39 +0800

    webpack配置优化

---
+++ build/antdv-icons.js
@@ -0,0 +1,86 @@
+// alert form progress
+export { default as CheckCircleFill } from '@ant-design/icons/lib/fill/CheckCircleFill';
+// alert form modal progress result
+export { default as CheckCircleOutline } from '@ant-design/icons/lib/outline/CheckCircleOutline';
+// alert
+export { default as InfoCircleFill } from '@ant-design/icons/lib/fill/InfoCircleFill';
+// alert modal
+export { default as InfoCircleOutline } from '@ant-design/icons/lib/outline/InfoCircleOutline';
+// alert date-picker form input progress result select time-picker transfer tree-select
+export { default as CloseCircleFill } from '@ant-design/icons/lib/fill/CloseCircleFill';
+// alert date-picker form modal notification progress
+export { default as CloseCircleOutline } from '@ant-design/icons/lib/outline/CloseCircleOutline';
+// alert form popconfirm result
+export { default as ExclamationCircleFill } from '@ant-design/icons/lib/fill/ExclamationCircleFill';
+// alert form modal
+export { default as ExclamationCircleOutline } from '@ant-design/icons/lib/outline/ExclamationCircleOutline';
+// breadcrumb color-picker input-number select table tabs tree-select
+export { default as DownOutline } from '@ant-design/icons/lib/outline/DownOutline';
+// button form input message select switch timeline tree tree-select upload
+export { default as LoadingOutline } from '@ant-design/icons/lib/outline/LoadingOutline';
+// cascader
+export { default as RedoOutline } from '@ant-design/icons/lib/outline/RedoOutline';
+// collapse dropdown layout pagination tabs
+export { default as RightOutline } from '@ant-design/icons/lib/outline/RightOutline';
+// date-picker
+export { default as CalendarOutline } from '@ant-design/icons/lib/outline/CalendarOutline';
+// drawer progress select tabs tag tree-select
+export { default as CloseOutline } from '@ant-design/icons/lib/outline/CloseOutline';
+// dropdown
+export { default as EllipsisOutline } from '@ant-design/icons/lib/outline/EllipsisOutline';
+// input upload
+export { default as EyeOutline } from '@ant-design/icons/lib/outline/EyeOutline';
+// input
+export { default as EyeInvisibleOutline } from '@ant-design/icons/lib/outline/EyeInvisibleOutline';
+// input transfer
+export { default as SearchOutline } from '@ant-design/icons/lib/outline/SearchOutline';
+// input-number tabs
+export { default as UpOutline } from '@ant-design/icons/lib/outline/UpOutline';
+// layout
+export { default as BarsOutline } from '@ant-design/icons/lib/outline/BarsOutline';
+// layout pagination tabs
+export { default as LeftOutline } from '@ant-design/icons/lib/outline/LeftOutline';
+// modal
+export { default as QuestionCircleOutline } from '@ant-design/icons/lib/outline/QuestionCircleOutline';
+// page-header
+export { default as ArrowLeftOutline } from '@ant-design/icons/lib/outline/ArrowLeftOutline';
+// pagination
+export { default as DoubleLeftOutline } from '@ant-design/icons/lib/outline/DoubleLeftOutline';
+// pagination
+export { default as DoubleRightOutline } from '@ant-design/icons/lib/outline/DoubleRightOutline';
+// progress select steps
+export { default as CheckOutline } from '@ant-design/icons/lib/outline/CheckOutline';
+// rate
+export { default as StarFill } from '@ant-design/icons/lib/fill/StarFill';
+// result
+export { default as WarningFill } from '@ant-design/icons/lib/fill/WarningFill';
+// table
+export { default as FilterFill } from '@ant-design/icons/lib/fill/FilterFill';
+// table
+export { default as CaretUpFill } from '@ant-design/icons/lib/fill/CaretUpFill';
+// table tree tree-select
+export { default as CaretDownFill } from '@ant-design/icons/lib/fill/CaretDownFill';
+// tabs
+export { default as PlusOutline } from '@ant-design/icons/lib/outline/PlusOutline';
+// time-picker
+export { default as ClockCircleOutline } from '@ant-design/icons/lib/outline/ClockCircleOutline';
+// tree
+export { default as FileOutline } from '@ant-design/icons/lib/outline/FileOutline';
+// tree
+export { default as FolderOpenOutline } from '@ant-design/icons/lib/outline/FolderOpenOutline';
+// tree
+export { default as FolderOutline } from '@ant-design/icons/lib/outline/FolderOutline';
+// tree
+export { default as MinusSquareOutline } from '@ant-design/icons/lib/outline/MinusSquareOutline';
+// tree
+export { default as PlusSquareOutline } from '@ant-design/icons/lib/outline/PlusSquareOutline';
+// upload
+export { default as PaperClipOutline } from '@ant-design/icons/lib/outline/PaperClipOutline';
+// upload
+export { default as PictureTwoTone } from '@ant-design/icons/lib/twotone/PictureTwoTone';
+// upload
+export { default as FileTwoTone } from '@ant-design/icons/lib/twotone/FileTwoTone';
+// upload
+export { default as DeleteOutline } from '@ant-design/icons/lib/outline/DeleteOutline';
+// upload
+export { default as DownloadOutline } from '@ant-design/icons/lib/outline/DownloadOutline';
--- vue.config.js
+++ vue.config.js
@@ -1,107 +1,57 @@
+const path = require('path');
+const isProduction = process.env.NODE_ENV === 'production';
+const webpack = require('webpack');
+
+const AntDesignIconsPath = './build/antdv-icons.js';
 
 module.exports = {
-    // 选项
-    //  基本路径
-    publicPath: process.env.NODE_ENV === 'production' ? '' : '',
-    //  构建时的输出目录
-    outputDir: "dist",
-    //  放置静态资源的目录
-    // assetsDir: "static",
-    //  html 的输出路径
-    indexPath: "index.html",
-    //文件名哈希
-    filenameHashing: false,
-    // 配置 webpack-dev-server 行为。
+    outputDir: 'dist',
+    // 自定义html模板cpoy后目标的路径(相对 outputDir 的路径)或者修改文件名字、后缀
+    indexPath: './index.ftl',
+    // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+    assetsDir: './assets',
+    publicPath: isProduction ? '' : '',
+    // babel-loader 打包时强制重新编译打包某些依赖
+    transpileDependencies: ['ant-design-vue'],
+    productionSourceMap: false,
+    // 不使用 eslint 检查
+    lintOnSave: false,
+    configureWebpack: {
+        plugins: [
+            // moment 打包体积优化
+            // new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn/),
+        ],
+        resolve: {
+            // 创建 import 或 require 的别名，来确保模块引入变得更简单，引入的时候可以不写相对路径
+            alias: {
+                // antd 打包体积优化，去掉不需要的icons
+                '@ant-design/icons/lib/dist$': path.resolve(__dirname, AntDesignIconsPath),
+            },
+        },
+        externals: {},
+    },
+    chainWebpack: (config) => {
+        // 设置路径别名
+        config.resolve.alias.set('@', path.resolve(__dirname, './src')).set('@comB', '@/components/business').set('@comC', '@/components/common');
+
+        // 打包html文件关闭压缩
+        config.plugin('html').tap((args) => {
+            args[0].mminify = false;
+            return args;
+        });
+
+        // 将svg文件剔除最终打包体积
+        const svgRule = config.module.rule('svg');
+        svgRule.uses.clear();
+        svgRule.use('url-loader').loader('url-loader');
+    },
     devServer: {
-        // open: process.platform === 'darwin',
-        // host: '0.0.0.0',
-        // port: 8080,
-        // https: false,
-        // hotOnly: false,
-        // 查阅 https://github.com/vuejs/vue-docs-zh-cn/blob/master/vue-cli/cli-service.md#配置代理
         proxy: {
             '/api': {
-                target: "http://mywebsite.com",
+                target: 'http://gzh.com',
+                ws: true,
                 changeOrigin: true,
-                secure: false,
-                pathRewrite: {
-                    "^/api": ""
-                }
             },
-            '/foo': {
-                target: '<other_url>'
-            }
-        }, // string | Object
-        before: app => { }
+        },
     },
-    //  是否为生产环境构建生成 source map？
-    productionSourceMap: false,
-    // //用于多页配置，默认是 undefined
-    // pages: {
-    //     index: {
-    //         // page 的入口文件
-    //         entry: 'src/index/main.js',
-    //         // 模板文件
-    //         template: 'public/index.html',
-    //         // 在 dist/index.html 的输出文件
-    //         filename: 'index.html',
-    //         // 当使用页面 title 选项时，
-    //         // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-    //         title: 'Index Page',
-    //         // 在这个页面中包含的块，默认情况下会包含
-    //         // 提取出来的通用 chunk 和 vendor chunk。
-    //         chunks: ['chunk-vendors', 'chunk-common', 'index']
-    //     },
-    //     // 当使用只有入口的字符串格式时，
-    //     // 模板文件默认是 `public/subpage.html`
-    //     // 如果不存在，就回退到 `public/index.html`。
-    //     // 输出文件默认是 `subpage.html`。
-    //     subpage: 'src/subpage/main.js'
-    // },
-    //  是否在保存的时候使用 `eslint-loader` 进行检查。
-    lintOnSave: false,
-    // //  是否使用带有浏览器内编译器的完整构建版本
-    // runtimeCompiler: false,
-    // //  babel-loader 默认会跳过 node_modules 依赖。
-    transpileDependencies: [
-        'ant-design-vue'
-    ],
-    // //  设置生成的 HTML 中 <link rel="stylesheet"> 和 <script> 标签的 crossorigin 属性。
-    // crossorigin: "",
-    // //  在生成的 HTML 中的 <link rel="stylesheet"> 和 <script> 标签上启用 Subresource Integrity (SRI)。
-    // integrity: false,
-    // //  调整内部的 webpack 配置
-    // configureWebpack: () => { }, //(Object | Function)
-    // chainWebpack: () => { },
-    // // CSS 相关选项
-    // css: {
-    //     // 将组件内的 CSS 提取到一个单独的 CSS 文件 (只用在生产环境中)
-    //     // 也可以是一个传递给 `extract-text-webpack-plugin` 的选项对象
-    //     extract: true,
-    //     // 是否开启 CSS source map？
-    //     sourceMap: false,
-    //     // 为预处理器的 loader 传递自定义选项。比如传递给
-    //     // Css-loader 时，使用 `{ Css: { ... } }`。
-    //     loaderOptions: {
-    //         css: {
-    //             // 这里的选项会传递给 css-loader
-    //         },
-    //         postcss: {
-    //             // 这里的选项会传递给 postcss-loader
-    //         }
-    //     },
-    //     // 为所有的 CSS 及其预处理文件开启 CSS Modules。
-    //     // 这个选项不会影响 `*.vue` 文件。
-    //     modules: false
-    // },
-    // // 在生产环境下为 Babel 和 TypeScript 使用 `thread-loader`
-    // // 在多核机器下会默认开启。
-    // parallel: require('os').cpus().length > 1,
-    // // PWA 插件的选项。
-    // // 查阅 https://github.com/vuejs/vue-docs-zh-cn/blob/master/vue-cli-plugin-pwa/README.md
-    // pwa: {},
-    // // 三方插件的选项
-    // pluginOptions: {
-    //     // ...
-    // }
-}
+};
