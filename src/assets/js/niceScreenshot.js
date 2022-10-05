728e364c6cf4631283ac5ce573f64cbfb115e99e
ApexLegend <cutelittlecat@163.com>
2022-10-05 13:38:38 +0800

    inf

--- src/assets/js/niceScreenshot.js
+++ src/assets/js/niceScreenshot.js
@@ -1,3 +1,8 @@
+/*
+    原作者：GZHIH
+    github地址：https://gitee.com/GZHIH/nice-screenshot
+    码云地址：https://github.com/GZHIH/niceScreenshot
+*/
 
 class Dom2Img {
     /*
@@ -15,75 +20,76 @@ class Dom2Img {
     */
     constructor(dom, option) {
         if (!dom) {
-            console.error('缺少导出的DOM！')
+            console.error('缺少导出的DOM！');
         }
         const option_ = option || {
             name: new Date().getTime(),
             customStyle: '',
             size: 1,
-        }
-        const { name, customStyle, size, width, height } = option_
+        };
+        const { name, customStyle, size, width, height } = option_;
 
-        this.originDom = dom
-        this.name = name || new Date().getTime()
-        this.customStyle = customStyle || ''
-        this.size = size || 1
-        this.width = width || this.originDom.offsetWidth
-        this.height = height || this.originDom.offsetHeight
+        this.originDom = dom;
+        this.name = name || new Date().getTime();
+        this.customStyle = customStyle || '';
+        this.size = size || 1;
+        this.width = width || this.originDom.offsetWidth;
+        this.height = height || this.originDom.offsetHeight;
 
-        this.cloneDom = dom.cloneNode(true)
-        this.cloneDom.setAttribute('xmlns', "http://www.w3.org/1999/xhtml")
+        this.cloneDom = dom.cloneNode(true);
+        this.cloneDom.setAttribute('xmlns', 'http://www.w3.org/1999/xhtml');
 
         this.imgNodes = {
             origin: [],
-            clone: []
-        }
+            clone: [],
+        };
         this.canvasNodes = {
             origin: [],
-            clone: []
-        }
+            clone: [],
+        };
 
-        const canvas = document.createElement("canvas")
+        const canvas = document.createElement('canvas');
         this.CANVAS = {
             canvas,
-            context: canvas.getContext("2d")
-        }
-        this.styleSheet = ''
-        this.svgDomText = ''
+            context: canvas.getContext('2d'),
+        };
+        this.styleSheet = '';
+        this.svgDomText = '';
 
-        this.addImgNodes(this.originDom, this.cloneDom)
-        this.renderImg()
-        this.render()
-        this.download()
+        this.addImgNodes(this.originDom, this.cloneDom);
+        this.renderImg();
+        this.render();
+        this.download();
     }
     draw(img) {
-        const { canvas, context } = this.CANVAS
-        const { size } = this
-        const width = img.width * size, height = img.height * size
+        const { canvas, context } = this.CANVAS;
+        const { size } = this;
+        const width = img.width * size,
+            height = img.height * size;
 
-        canvas.width = width
-        canvas.height = height
-        context.clearRect(0, 0, width, height)
-        context.drawImage(img, 0, 0, width, height)
+        canvas.width = width;
+        canvas.height = height;
+        context.clearRect(0, 0, width, height);
+        context.drawImage(img, 0, 0, width, height);
     }
     // 处理图片性质的资源
     addImgNodes(originDom, cloneDom) {
-        const { imgNodes, canvasNodes, addImgNodes } = this
+        const { imgNodes, canvasNodes, addImgNodes } = this;
         for (let i = 0; i < originDom.childNodes.length; i++) {
-            const originNode = originDom.childNodes[i]
-            const cloneNode = cloneDom.childNodes[i]
+            const originNode = originDom.childNodes[i];
+            const cloneNode = cloneDom.childNodes[i];
             // console.log(cloneNode.tagName)
             if (originNode.tagName === 'IMG') {
-                imgNodes.origin.push(originNode)
-                imgNodes.clone.push(cloneNode)
+                imgNodes.origin.push(originNode);
+                imgNodes.clone.push(cloneNode);
             }
             if (originNode.tagName === 'CANVAS') {
-                canvasNodes.origin.push(originNode)
-                canvasNodes.clone.push(cloneNode)
+                canvasNodes.origin.push(originNode);
+                canvasNodes.clone.push(cloneNode);
             }
             // 过滤 text 节点 script 节点，递归
-            if (originNode.nodeType !== 3 && originNode.nodeName !== "SCRIPT" && originNode.nodeName !== "svg" && originNode.nodeName !== "#comment") {
-                addImgNodes.apply(this, [originNode, cloneNode])
+            if (originNode.nodeType !== 3 && originNode.nodeName !== 'SCRIPT' && originNode.nodeName !== 'svg' && originNode.nodeName !== '#comment') {
+                addImgNodes.apply(this, [originNode, cloneNode]);
                 // or
                 // this.addImgNodes(originNode, cloneNode)
             }
@@ -91,81 +97,81 @@ class Dom2Img {
     }
     // 将图片性质的资源渲染到canvas上
     renderImg() {
-        const { imgNodes, canvasNodes } = this
-        const { canvas } = this.CANVAS
+        const { imgNodes, canvasNodes } = this;
+        const { canvas } = this.CANVAS;
 
         imgNodes.clone.forEach((cloneImgItem, imgIndex) => {
-            const originImgItem = imgNodes.origin[imgIndex]
-            this.draw(originImgItem)
-            cloneImgItem.src = canvas.toDataURL()
-        })
+            const originImgItem = imgNodes.origin[imgIndex];
+            this.draw(originImgItem);
+            cloneImgItem.src = canvas.toDataURL();
+        });
         canvasNodes.clone.forEach((cloneCanvasItem, canvasIndex) => {
-            const originCanvasItem = canvasNodes.origin[canvasIndex]
+            const originCanvasItem = canvasNodes.origin[canvasIndex];
             // console.log(originCanvasItem, cloneCanvasItem)
 
-            const IMG = document.createElement('img')
-            IMG.setAttribute('src', originCanvasItem.toDataURL())
-            IMG.setAttribute('width', originCanvasItem.offsetWidth)
-            IMG.setAttribute('height', originCanvasItem.offsetHeight)
+            const IMG = document.createElement('img');
+            IMG.setAttribute('src', originCanvasItem.toDataURL());
+            IMG.setAttribute('width', originCanvasItem.offsetWidth);
+            IMG.setAttribute('height', originCanvasItem.offsetHeight);
 
-            cloneCanvasItem.parentNode.replaceChild(IMG, cloneCanvasItem)
-        })
+            cloneCanvasItem.parentNode.replaceChild(IMG, cloneCanvasItem);
+        });
     }
     render() {
         // 页面所有的样式塞入
-        const { originDom, cloneDom, width, height } = this
+        const { originDom, cloneDom, width, height } = this;
 
-        const { styleSheets } = document
-        let styleSheet = "<style>"
+        const { styleSheets } = document;
+        let styleSheet = '<style>';
         for (let i = 0; i < styleSheets.length; i++) {
-            const cssList = styleSheets[i].cssRules
+            const cssList = styleSheets[i].cssRules;
             for (let index = 0; index < cssList.length; index++) {
                 if (cssList[index].selectorText) {
                     // 测试在svg中添加无用的css样式不影响最终导出的图片的大小, 所以这里无需将指定区域的样式单独摘出来塞入（也比较麻烦，比如遇到使用第三方组件的情况），直接一锅端全部塞入绝对ok
                     // 如果这里比较影响性能，可以尝试将指定区域的样式单独摘出来塞入 style 中
-                    styleSheet += cssList[index].cssText
+                    styleSheet += cssList[index].cssText;
                 }
             }
         }
-        styleSheet += this.customStyle
-        styleSheet += "</style>"
-        this.styleSheet = styleSheet
+        styleSheet += this.customStyle;
+        styleSheet += '</style>';
+        this.styleSheet = styleSheet;
 
-        const foreignObjectText = encodeURIComponent(`${this.styleSheet}${new XMLSerializer().serializeToString(cloneDom)}`)
+        const foreignObjectText = encodeURIComponent(`${this.styleSheet}${new XMLSerializer().serializeToString(cloneDom)}`);
         const svgDomText = `
             data:image/svg+xml;charset=utf-8,
             <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}">
                 <foreignObject x="0" y="0" width="100%" height="100%">${foreignObjectText}</foreignObject>
             </svg>
-        `
-        this.svgDomText = svgDomText
+        `;
+        this.svgDomText = svgDomText;
     }
     download() {
-        const { CANVAS, name } = this
-        const { canvas } = CANVAS
+        const { CANVAS, name } = this;
+        const { canvas } = CANVAS;
 
-        const finalImg = new Image()
+        const finalImg = new Image();
         finalImg.onload = () => {
-            this.draw(finalImg)
-            downloadImgByBase64(canvas.toDataURL('image/png', 1), name)
-        }
+            this.draw(finalImg);
+            downloadImgByBase64(canvas.toDataURL('image/png', 1), name);
+        };
         // 给图片对象添加图片地址，触发onload
-        finalImg.src = this.svgDomText
+        finalImg.src = this.svgDomText;
         // document.body.appendChild(finalImg)
     }
 }
 
 const downloadImgByBase64 = (base64URL, fileName) => {
-    const $a = document.createElement('a')
-    $a.download = `${fileName}`
-    $a.target = '_blank'
-    $a.href = base64URL
+    const $a = document.createElement('a');
+    $a.download = `${fileName}`;
+    $a.target = '_blank';
+    $a.href = base64URL;
     const evt = new MouseEvent('click', {
         view: window,
         bubbles: true,
-        cancelable: false
-    })
-    $a.dispatchEvent(evt)
-}
+        cancelable: false,
+    });
+    $a.dispatchEvent(evt);
+};
 
-export default (dom, option) => new Dom2Img(dom, option)
+export default (dom, option) => new Dom2Img(dom, option);
